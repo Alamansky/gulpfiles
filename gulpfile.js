fbe41606345338e4acaa468074fcb108681950ef
Andrew Lamansky <andrewlamansky@gmail.com>
2020-02-18 10:03:57 -0500

    add sass-browsersync gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,28 @@
 // dependencies
 const gulp = require("gulp");
 const sass = require("gulp-sass");
+const browserSync = require("browser-sync").create();
+var reload = browserSync.reload;
 
+// dev server vars
+const PORT = 3000;
 const BASEDIR = "./public";
 
-// path variables
+// path vars
 const path = {
+  html: `${BASEDIR}/*.html`,
   css: `${BASEDIR}/`,
-  sass: `${BASEDIR}/*.scss`
+  sass: `${BASEDIR}/*.scss`,
+  js: `${BASEDIR}/*.js`
 };
+
+// dev server task
+gulp.task("serve", function() {
+  browserSync.init({
+    server: BASEDIR,
+    port: PORT
+  });
+});
 
 // sass task
 gulp.task("sass", function() {
@@ -16,11 +30,18 @@ gulp.task("sass", function() {
     .src(path.sass)
     .pipe(sass())
     .pipe(sass().on("error", sass.logError))
-    .pipe(gulp.dest(path.css));
+    .pipe(gulp.dest(path.css))
+    .pipe(reload({ stream: true }));
+  // ^ reload browsersync after transform
 });
 
-// default ask - call from terminal with `npx gulp`
-// `gulp.series` maps string "sass" to func called `on` method
+// defaultask - call from terminal with `npx gulp`
 gulp.task("default", function() {
-  gulp.watch(path.sass).on("change", gulp.series("sass"));
+  // dev server is independant task - `gulp.series` returns func from string which is then executed
+  gulp.series("serve")();
+
+  // abbreviated gulp.watch syntax for sass watch
+  gulp.watch(path.sass, gulp.series("sass"));
+  gulp.watch(path.html).on("change", reload);
+  gulp.watch(path.js).on("change", reload);
 });
