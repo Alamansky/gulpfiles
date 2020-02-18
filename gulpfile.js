bb12b6d6650aee3ba6b746a38798a0e159c3af94
Andrew Lamansky <andrewlamansky@gmail.com>
2020-02-18 14:37:01 -0500

    update imagemin branch

--- gulpfile.js
+++ gulpfile.js
@@ -1,47 +1,18 @@
-// dependencies
 const gulp = require("gulp");
-const sass = require("gulp-sass");
-const browserSync = require("browser-sync").create();
-var reload = browserSync.reload;
+const imagemin = require("gulp-imagemin");
 
-// dev server vars
-const PORT = 3000;
 const BASEDIR = "./public";
 
-// path vars
 const path = {
-  html: `${BASEDIR}/*.html`,
-  css: `${BASEDIR}/`,
-  sass: `${BASEDIR}/*.scss`,
-  js: `${BASEDIR}/*.js`
+  raw: `${BASEDIR}/raw_imgs/*.jpg`,
+  opt: `${BASEDIR}/opt_imgs/`
 };
 
-// dev server task
-gulp.task("serve", function() {
-  browserSync.init({
-    server: BASEDIR,
-    port: PORT
-  });
-});
+//paths var
 
-// sass task
-gulp.task("sass", function() {
-  return gulp
-    .src(path.sass)
-    .pipe(sass())
-    .pipe(sass().on("error", sass.logError))
-    .pipe(gulp.dest(path.css))
-    .pipe(reload({ stream: true }));
-  // ^ reload browsersync after transform
-});
-
-// defaultask - call from terminal with `npx gulp`
-gulp.task("default", function() {
-  // dev server is independant task - `gulp.series` returns func from string which is then executed
-  gulp.series("serve")();
-
-  // abbreviated gulp.watch syntax for sass watch
-  gulp.watch(path.sass, gulp.series("sass"));
-  gulp.watch(path.html).on("change", reload);
-  gulp.watch(path.js).on("change", reload);
-});
+gulp.task("default", () =>
+  gulp
+    .src(path.raw)
+    .pipe(imagemin({ verbose: true }))
+    .pipe(gulp.dest(path.opt))
+);
